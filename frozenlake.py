3fe0151af530f2d476af45549256bc56b92964b9
ptawethe2nd <96425546+ptawethe2nd@users.noreply.github.com>
2022-01-03 16:13:33 +0000

    completed method p & r, but going to do little finishing touches

--- frozenlake.py
+++ frozenlake.py
@@ -15,27 +15,26 @@ class FrozenLake(Environment):
 
     def __init__(self, lake, slip, max_steps, seed=None):
 
-        lake =  [ ['&', '.', '.', '.'],
-                  ['.', '#', '.', '#'],
-                  ['.', '.', '.', '#'],
-                  ['#', '.', '.', '$']
-                ]
-
         # start (&), frozen (.), hole (#), goal ($)
         self.lake = np.array(lake)
         self.lake_flat = self.lake.reshape(-1)
         
         self.slip = slip
         
-        self.n_states = self.lake.size + 1
-        self.n_actions = 4
+        n_states = self.lake.size + 1
+        n_actions = 4
         
-        pi = np.zeros(self.n_states, dtype=float)
+        pi = np.zeros(n_states, dtype=float)
         pi[np.where(self.lake_flat == '&')[0]] = 1.0
         
-        self.absorbing_state = self.n_states - 1
+        self.absorbing_state = n_states - 1
 
         self.action_probabilities = np.load('p.npy')
+
+        self.width = len(lake[0])
+        self.height = len(lake)
+
+        Environment.__init__(self, n_states, n_actions, max_steps, pi, seed)
         
         # TODO:
         
@@ -50,29 +49,44 @@ class FrozenLake(Environment):
     def p(self, next_state, state, action):
         # TODO:
 
-        expcted_state = self.take_action(state, action)
-        return expcted_state == next_state
+        #expected_state = self.take_action(state, action)
+        #return expected_state == next_state
+
+        possible_adjacent_states = []
+        distributional_probabilities = np.zeros(self.n_states)
+
+        for i in range(self.n_actions):
+            pas = self.take_action(state, i) 
+            possible_adjacent_states.append(pas)
+
+        shared_slip_probability = self.slip/len(possible_adjacent_states)
+        
+        for adjacent_state in possible_adjacent_states:
+            distributional_probabilities[adjacent_state] += shared_slip_probability
+
+        next_expected_state = self.take_action(state, action)
+        distributional_probabilities[next_expected_state] += (1 - self.slip)
+
+        return distributional_probabilities[next_state]
+
     
     #The method r returns the expected reward in having transitioned from state to next state given action
     def r(self, next_state, state, action):
         # TODO:
         
-        if state in self.goal_states:
+        if self.goal_state(state):
            return 1
-
-        if state in self.trap_states:
-           return -1
 
         return 0
 
     #NEW METHOD take_action returns the coordinates of the new state after taking an action
     def take_action(self, state, action):
-        
-        if self.is_endstate(state):
+
+        if state == self.absorbing_state:
             return state
 
-        if state in self.absorbing_state:
-            return self.endstate()
+        if state == self.hole_state(state) or self.goal_state(state):
+            return self.absorbing_state
 
         state_coordinates = self.state_to_coordinates(state)
         action_coordinates = self.action_to_coordinates(action)
@@ -96,47 +110,48 @@ class FrozenLake(Environment):
         if (coordinates[1] < 0) or (coordinates[1] >= self.height):
             return False
 
-        coordinates_state = self.coordinates_to_state(coordinates)
-        return coordinates_state not in self.wall_indexes
+        return True    
+
+
+    def hole_state(self, state):
+        if state == self.absorbing_state:
+            return False
+
+        return self.lake_flat[int(state)] == '#'
+
+    def goal_state(self, state):
+        if state == self.absorbing_state:
+            return False
+            
+        return self.lake_flat[int(state)] == '$'
 
     #NEW METHOD transfroms the states to coordinates on the environment
     def state_to_coordinates(self,state):
-
-        assert state < self.grid_size, "{0} not on grid".format(state)
 
         x_index = state % self.width
         y_index = (state - x_index) / self.width
         return [x_index, y_index]
 
     #NEW METHOD transfroms the states to coordinates on the environment
-    def coordinaes_to_state(self, coordinates):
+    def coordinates_to_state(self, coordinates):
         return (coordinates[1] * self.width) + coordinates[0]
 
     #NEW METHOD transfroms the actions to coordinates on the environment
     def action_to_coordinates(self, action):
 
         if action == 0: #UP
-           return [0, 1]
-
-        if action == 1: #RIGHT
-           return [1, 0]
-
-        if action == 2: #DOWN
            return [0, -1]
 
-        if action == 3: #LEFT
+        if action == 1: #LEFT
            return [-1, 0]
 
+        if action == 2: #DOWN
+           return [0, 1]
+
+        if action == 3: #RIGHT
+           return [1, 0]
+
         return [0, 0]
-        
-    def is_endstate(self, state):
-        return state == self.endstate()
-
-    def endstate(self):
-        return self.grid_size
-
-    def set_renderer(self, renderer):
-        self.renderer = renderer
 
    
    
@@ -179,5 +194,16 @@ def play(env):
         state, r, done = env.step(actions.index(c))
         
         env.render()
-        print('Reward: {0}.'.format(r))
+        print('Reward: {0}, done:{1}.'.format(r, done))
 
+seed = 0
+    
+# Small lake
+lake = [['&', '.', '.', '.'],
+        ['.', '#', '.', '#'],
+        ['.', '.', '.', '#'],
+        ['#', '.', '.', '$']]
+
+env = FrozenLake(lake, slip=0.1, max_steps=16, seed=seed)
+
+play(env)
