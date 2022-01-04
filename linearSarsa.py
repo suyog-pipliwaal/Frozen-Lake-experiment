55094eebaaafb867e2ccc520fc62eb198cf1aae4
Suyog Pipliwal <suyogpipliwal@gmail.com>
2022-01-04 15:46:18 +0000

    linear q learning and linear sarsa done

--- linearSarsa.py
+++ linearSarsa.py
@@ -5,41 +5,44 @@ import numpy as np
 from chooseAction import choose_action
 def linear_sarsa(env, max_episodes, eta, gamma, epsilon, seed=None):
     random_state = np.random.RandomState(seed)
-    
+
     eta = np.linspace(eta, 0, max_episodes)
     epsilon = np.linspace(epsilon, 0, max_episodes)
-    
+
     theta = np.zeros(env.n_features)
-    
+
     for i in range(max_episodes):
         features = env.reset()
-        
+
         q = features.dot(theta)
-        print(features.shape)
-        # print("----->", q)
-        # state = np.where(features == 1)
-        # print("state = ", state)
-        print(env.state)
-    #     state = env.state # change this line to get current state:
-    #     action = choose_action(epsilon[i], q, random_state)
-    #     done = False
-    #     while not done:
-    #         next_state, reward, done = env.step(action)
-    #         print("next_state", next_state)
-    #         next_action = choose_action(epsilon[i], q, next_state)
-            
-    #         print("next action", next_action)
-    #         delta  = reward + gamma*q[next_state, next_action] - q[state, action]
-    #         for index in range(len(theta)):
-    #             theta[index] = theta[index] + eta[i]*delta*features[state, action]
-    #         state = next_state
-    #         action = next_action
-    # return theta
+        if random_state.rand() < epsilon[i]:
+            action = random_state.choice(env.n_actions)
+        else:
+            qmax = max(q)
+            best = [a for a in range(env.n_actions) if np.allclose(qmax, q[a])]
+            action = random_state.choice(best)
+        done = False
+        while not done:
+            next_features, reward, done = env.step(action)
+            next_q = next_features.dot(theta)
+            if random_state.rand() < epsilon[i]:
+                next_action = random_state.choice(env.n_actions)
+            else:
+                qmax = max(next_q)
+                best = [na for na in range(env.n_actions) if np.allclose(qmax, next_q[na])]
+                next_action = random_state.choice(best)
+
+            delta  = reward + gamma*next_q[next_action] - q[action]
+            theta = theta + eta[i]*delta*features[action,:]
+
+            features = next_features
+            q = features.dot(theta)
+            action = next_action
+    return theta
 
 if __name__ =='__main__':
     seed = 0
-    # max_episodes = 2000
-    max_episodes = 1
+    max_episodes = 2000
     eta = 0.5
     epsilon = 0.5
     gamma = 0.9
@@ -50,11 +53,11 @@ if __name__ =='__main__':
 
     env = FrozenLake(lake, slip=0.1, max_steps=16, seed=seed)
     linear_env = LinearWrapper(env)
-    
+
     print('## Linear Sarsa')
 
     parameters = linear_sarsa(linear_env, max_episodes, eta, gamma, epsilon, seed=seed)
     policy, value = linear_env.decode_policy(parameters)
     linear_env.render(policy, value)
-    
+
     print('')
