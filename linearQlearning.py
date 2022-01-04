9288b80b3f0b95e476fcde6a1d2372e3a0882c6c
Suyog Pipliwal <suyogpipliwal@gmail.com>
2022-01-04 14:29:00 +0000

    some more changes

---
+++ linearQlearning.py
@@ -0,0 +1,15 @@
+import numpy as np
+def linear_q_learning(env, max_episodes, eta, gamma, epsilon, seed=None):
+    random_state = np.random.RandomState(seed)
+
+    eta = np.linspace(eta, 0, max_episodes)
+    epsilon = np.linspace(epsilon, 0, max_episodes)
+
+    theta = np.zeros(env.n_features)
+
+    for i in range(max_episodes):
+        features = env.reset()
+        state = random_state
+        for action in 
+
+    return theta
